d2d1f76ad0709217a00fd62b2f0f0fe53a0b8260
shopzoid <shopzoid.id@gmail.com>
2021-10-28 07:59:58 +0700

    first commit

--- daftar/assets/script.js
+++ daftar/assets/script.js
@@ -87,7 +87,7 @@ class FormValidator {
 
 
 const form = document.querySelector('.form');
-const fields = ["name", "Instagram", "password", "password_confirmation"];
+const fields = ["name", "email", "phone", "instagram"];
 
 const validator = new FormValidator(form, fields);
 validator.initialize();
